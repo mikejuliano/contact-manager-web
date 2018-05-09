7d960f5068bb91f20b73860cf9212368916004fc
Mike Juliano <mjuliano@buildinmotion.com>
2018-05-09 13:20:09 -0400

    displaying contacts on home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,12 +2,41 @@
 import React, {Component} from 'react';
 
 export default class HomePage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {contacts: []};
+  }
+
+  componentDidMount() {
+    this.fetchContacts();
+  }
+
+  fetchContacts() {
+    const url = '/api/v1/contacts/';
+    const options = {method: 'GET'};
+    const headers = {'Content-Type': 'application/json'};
+    return fetch(url, {headers, ...options})
+      .then(r => r.json())
+      .then(contacts => {
+        this.setState({contacts});
+      });
+  }
+
   render() {
     const {user} = this.props;
     return (
       <div>
-        <h1>Home Page</h1>
-        <p>{ user }</p>
+        <div>
+          <h1>Home Page</h1>
+          <p>{ user }</p>
+        </div>
+        <div>
+          <h3>Contacts</h3>
+          { this.state.contacts.map((contact, index) => <div key={ index }>
+            { `Name: ${contact.first_name} ${contact.last_name} | Email: ${contact.email}` }
+          </div>) }
+        </div>
       </div>
     );
   }

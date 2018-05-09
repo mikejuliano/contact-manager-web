007c1e7b82ade30d729484a504b98c94a15e750c
Mike Juliano <mjuliano@buildinmotion.com>
2018-05-09 18:02:08 -0400

    tweaked contact component

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,11 @@
+// Contact.js
+
 import React from 'react';
 
 export const Contact = ({contact}) => {
   return (
     <div>
-      { `Name: ${contact.first_name} ${contact.last_name} | Email: ${contact.email}` }
+      { ['Name:', contact.first_name, contact.last_name, '|', 'Email', contact.email].join(' ') }
     </div>
   );
 };

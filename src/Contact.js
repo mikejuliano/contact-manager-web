e55c07c33147021cef48fd2e13ade282c3f8f314
Mike Juliano <mjuliano@buildinmotion.com>
2018-05-09 17:52:22 -0400

    created contact component

---
+++ src/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Contact = ({contact}) => {
+  return (
+    <div>
+      { `Name: ${contact.first_name} ${contact.last_name} | Email: ${contact.email}` }
+    </div>
+  );
+};

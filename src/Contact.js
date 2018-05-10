873a1b48341962bbb602ffb549253aa45b2a5dd0
Mike Juliano <mjuliano@buildinmotion.com>
2018-05-10 09:34:12 -0400

    clenaup

--- src/Contact.js
+++ src/Contact.js
@@ -5,7 +5,10 @@ import React from 'react';
 export const Contact = ({contact}) => {
   return (
     <div>
-      { ['Name:', contact.first_name, contact.last_name, '|', 'Email', contact.email].join(' ') }
+      { [
+        'Name:', contact.first_name, contact.last_name, '|',
+        'Email', contact.email
+      ].join(' ') }
     </div>
   );
 };
